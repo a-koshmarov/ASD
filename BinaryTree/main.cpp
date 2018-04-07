006e2e35dcff9a6cb1fd40b3c55227df65f3dae6
a-koshmarov <35034325+a-koshmarov@users.noreply.github.com>
2018-04-08 00:33:30 +0300

    BinTree

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -85,37 +86,43 @@ private:
             }
 
             Node *p_node = node->parent_;
+//            cout << p_node->value_ << endl;
 
             while (p_node != NULL && node == p_node->right_) {
                 node = p_node;
-                p_node = p_node->parent_;
-            }
 
-//        delete node;
+                if (p_node != root){
+                    p_node = p_node->parent_;
+                } else {
+                    return NULL;
+                }
+            }
             return p_node;
         } else {
-            int min = 1000000001;
+            pair<bool, int> min = make_pair(false, 0);
             node = root;
             int n;
             while (node) {
                 if (node->value_ > value) {
                     node = node->left_;
                 } else {
-                    if (abs(node->value_ - value) < min) {
-                        min = abs(node->value_ - value);
+                    if (!min.first) {
+                        min.second = abs(node->value_ - value);
+                        min.first = true;
+                        n = node->value_;
+                    } else if (abs(node->value_ - value) < min.second) {
+                        min.second = abs(node->value_ - value);
                         n = node->value_;
                     }
                     node = node->right_;
                 }
             }
-            if (min < 1000000001) {
+            if (min.first) {
                 return (successor(n));
             } else {
                 Node *t = minimum(root);
-                if (t && t->value_ > value) {
+                if (t->value_ > value) {
                     return t;
-                } else {
-                    return NULL;
                 }
             }
         }
@@ -133,31 +140,39 @@ private:
 
             while (p_node != NULL && node == p_node->left_) {
                 node = p_node;
-                p_node = p_node->parent_;
+                if (p_node != root){
+                    p_node = p_node->parent_;
+                } else {
+                    return NULL;
+                }
             }
 
 //        delete node;
             return p_node;
         } else {
-            int min = 1000000001;
+            pair<bool, int> min = make_pair(false, 0);
             node = root;
             int n;
             while (node) {
                 if (node->value_ < value) {
                     node = node->right_;
                 } else {
-                    if (abs(node->value_ - value) < min) {
-                        min = abs(node->value_ - value);
+                    if (!min.first) {
+                        min.second = abs(node->value_ - value);
+                        min.first = true;
+                        n = node->value_;
+                    } else if (abs(node->value_ - value) < min.second) {
+                        min.second = abs(node->value_ - value);
                         n = node->value_;
                     }
                     node = node->left_;
                 }
             }
-            if (min < 1000000001) {
+            if (min.first) {
                 return (ancestor(n));
             } else {
                 Node *t = maximum(root);
-                if (t && t->value_ < value) {
+                if (t->value_ < value) {
                     return t;
                 }
             }
@@ -167,7 +182,7 @@ private:
     void deleteNode(int value) {
         Node *element  = search(root, value);
 
-        if (element != NULL) {
+        if (element) {
             if ((element == root) && (element->left_ == NULL) && (element->right_ == NULL)) {
                 root = NULL;
             } else if ((element == root) && (element->left_ == NULL)) {
@@ -223,21 +238,23 @@ public:
         } else return 0;
     }
 
-    int treeSuccessor(int value) {
+    pair<bool, int> treeSuccessor(int value) {
+        if (!root) return make_pair(false, 0);
         Node *n = successor(value);
         if (n) {
-            return n->value_;
+            return make_pair(true, n->value_);
         } else {
-            return 1000000001;
+            return make_pair(false, 0);
         }
     }
 
-    int treeAncestor(int value) {
+    pair<bool, int> treeAncestor(int value) {
+        if (!root) return make_pair(false, 0);
         Node *n = ancestor(value);
         if (n) {
-            return n->value_;
+            return make_pair(true, n->value_);
         } else {
-            return 1000000001;
+            return make_pair(false, 0);
         }
     }
 
@@ -270,7 +287,8 @@ public:
 int main() {
     Tree tree;
     string command;
-    int num , ret;
+    int num;
+    long long ret;
 
     ifstream fin("bstsimple.in");
     ofstream fout("bstsimple.out");
@@ -280,7 +298,7 @@ int main() {
         fin >> num;
         if (fin.eof()) break;
 
-        if (command == "insert") {
+        if (command == "insert" && !tree.treeSearch(num)) {
             tree.newNode(num);
         } else if (command == "delete") {
             tree.treeDelete(num);
@@ -291,16 +309,20 @@ int main() {
                 fout << "false" << endl;
             }
         } else if (command == "next") {
-            ret = tree.treeSuccessor(num);
-            if (ret!=1000000001){
-                fout << ret << endl;
+            bool ret1;
+            int ret2;
+            tie(ret1, ret2) = tree.treeSuccessor(num);
+            if (ret1){
+                fout << ret2 << endl;
             } else {
                 fout << "none" << endl;
             }
         } else if (command == "prev") {
-            ret = tree.treeAncestor(num);
-            if (ret!=1000000001){
-                fout << ret << endl;
+            bool ret1;
+            int ret2;
+            tie(ret1, ret2) = tree.treeAncestor(num);
+            if (ret1){
+                fout << ret2 << endl;
             } else {
                 fout << "none" << endl;
             }
